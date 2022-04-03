ddb4abd025da12110fa5c2ee2839b1b190a2143d
umutsonmez <63147784+umutsonmez@users.noreply.github.com>
2022-04-03 14:28:35 +0300

    Add project files.

---
+++ ProgrammersBlog.Shared/Entites/Abstract/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Shared.Entites.Abstract
+{
+    public interface IEntity
+    {
+    }
+}
