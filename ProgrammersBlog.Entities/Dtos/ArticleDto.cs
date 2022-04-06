0d0c74f0db780316f3146fe34e7acc47b3f8499f
umutsonmez <63147784+umutsonmez@users.noreply.github.com>
2022-04-06 21:47:43 +0300

    added AutoMapper and article service

---
+++ ProgrammersBlog.Entities/Dtos/ArticleDto.cs
@@ -0,0 +1,17 @@
+﻿using ProgrammersBlog.Entities.Concrete;
+using ProgrammersBlog.Shared.Entites.Abstract;
+using ProgrammersBlog.Shared.Utilities.Results.ComplexTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Entities.Dtos
+{
+    public class ArticleDto:DtoGetBase
+    {
+        public Article Article { get; set; }
+        public override ResultStatus ResultStatus { get; set; } = ResultStatus.Success;
+    }
+}
